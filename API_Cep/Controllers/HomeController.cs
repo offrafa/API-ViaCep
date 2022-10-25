a3f7f420773c80ec85d093d0c5aa0ced421f72a2
Rafael <rafaeltavares462@gmail.com>
2022-10-24 21:05:27 -0300

    Adicionar arquivos de projeto.

---
+++ API_Cep/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using API_Cep.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace API_Cep.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ API_Cep/wwwroot/js/site.js
@@ -0,0 +1,30 @@
+﻿const cep = document.querySelector("#cep")
+
+
+const showData = (result)=>{
+    for(const campo in result){
+        if(document.querySelector("#" + campo)){
+            document.querySelector("#" + campo).value = result[campo]
+        }
+    }
+}
+
+
+
+// Função que quando o usuário tira o foco pega o que foi digitado
+cep.addEventListener("blur",(e)=>{
+    let search = cep.value.replace("-", "") // Retira os caractere especial 
+
+    const options = {
+        method: 'GET',
+        mode: 'cors',
+        Cache: 'default'
+    } 
+
+    fetch(`https://viacep.com.br/ws/${search}/json`, options)
+    .then(Response => {Response.json()
+        .then(data => showData(data))
+    })// Se dar carto
+    .catch(e => console.log('Deu erro '+ e,message))// Se dar erro
+
+})
